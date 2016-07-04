5ee63e19f28c608829953cead12ed25c6891c8ed
Esther Dama Kahindi <estherdamakahindi@Esthers-MacBook-Pro.local>
2016-07-04 15:52:45 +0300

    String Extension: Added tests and source code

---
+++ jasmine/spec/string-extension-tests.js
@@ -0,0 +1,64 @@
+'use strict';
+describe('String Extension', function() {
+
+  describe('Has Vowels', function() {
+    it('should return true if the string contains vowels', function() {
+      expect('hi'.hasVowels()).toBeTruthy();
+      expect('HI'.hasVowels()).toBeTruthy();
+    });
+  });
+
+  describe('To Upper', function() {
+    it('should return the string passed as all upper cases characters.', function() {
+      expect('hi'.toUpper()).toEqual('HI');
+      expect('HI'.toUpper()).toEqual('HI');
+    });
+  });
+
+  describe('To Lower', function() {
+    it('should return the string passed as all lower cases characters.', function() {
+      expect('HI'.toLower()).toEqual('hi');
+      expect('hi'.toLower()).toEqual('hi');
+    });
+
+  });
+
+  describe('Upper Case First Letter', function() {
+    it('should return the string passed with the First Character in Upper case.', function() {
+      expect('hi'.ucFirst()).toEqual('Hi');
+      expect('Hi'.ucFirst()).toEqual('Hi');
+      expect('HI'.ucFirst()).toEqual('Hi');
+    });
+  });
+
+  describe('is Question', function() {
+    it('should return true if the string is a question (ending with a question mark).', function() {
+      expect('hi?'.isQuestion()).toBeTruthy();
+      expect('hi? am good'.isQuestion()).toBeFalsy();
+    });
+  });
+
+  describe('Words', function() {
+    it('should return a list of the words in the string, as an Array. ', function() {
+      expect('this is a string.'.words()).toEqual(['this', 'is', 'a', 'string']);
+    });
+  });
+
+  describe('word Count', function() {
+    it('should return the number of words in the string.', function() {
+      expect('this is a string'.wordCount()).toEqual(4);
+    });
+  });
+
+  describe('To Currency', function() {
+    it('should return a currency representation of the string passed.', function() {
+      expect('11111.11'.toCurrency()).toEqual('11,111.11');
+    });
+  });
+
+  describe('From Currency', function() {
+    it('should return a number representation of the Currency String.', function() {
+      expect('11,111.11'.fromCurrency()).toEqual('11111.11');
+    });
+  });
+});
---
+++ jasmine/src/string-extension.js
@@ -0,0 +1,61 @@
+'use strict';
+String.prototype.hasVowels = function() {
+  //Returns true if the string contains vowels.
+
+  return /[aeiou]/i.test(this) ? true : false;
+};
+
+String.prototype.toUpper = function() {
+  //Returns the String in question but with all characters in upper cases as applicable.
+
+  return this.replace(/[a-z]/g, function(letter) {
+    //   return String.fromCharCode(letter.charCodeAt(0) & ~32);
+    // });
+    return String.fromCharCode(letter.charCodeAt(0) - 32);
+  });
+};
+
+String.prototype.toLower = function() {
+  //Returns the String in question but with all characters in their lower cases as applicable.
+
+  return this.replace(/[A-Z]/g, function(letter) {
+    return String.fromCharCode(letter.charCodeAt(0) + 32);
+  });
+};
+
+String.prototype.ucFirst = function() {
+  //Returns the String in question but changes the First Character to an Upper case.
+
+  return this[0].toUpper() + this.slice(1).toLower();
+};
+
+String.prototype.isQuestion = function() {
+  //Return true if the string is a question (ending with a question mark).
+
+  return /\?$/.test(this) ? true : false; //debug ending with a question mark only.
+};
+
+String.prototype.words = function() {
+  //Returns a list of the words in the string, as an Array. 
+
+  return this.replace(/\W/g, ' ').trim().split(' '); //debug this if there is a fullstoop in the middle of the string
+};
+
+String.prototype.wordCount = function() {
+  //Returns the number of words in the string. 
+
+  return this.words().length;
+};
+
+String.prototype.toCurrency = function() {
+  //Returns a currency representation of the String e.g 11111.11 should be represented as 11,111.11.
+ 
+  var string = this.split('.');
+  var format_string = string[0].split( /(?=(?:\d{3})+(?:\.|$))/g ).join( "," );
+  return format_string + '.' + string[1]
+};
+
+String.prototype.fromCurrency = function() {
+  //Returns a number representation of the Currency String e.g 11,111.11 should return 11111.11
+  return this.replace(/\,/g, '');
+};
